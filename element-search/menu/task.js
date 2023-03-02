229481db2d766e84af1b39bcb10c4c10494ce261
Овчинников Игорь <faceplaces@inbox.ru>
2023-03-02 21:31:09 +0300

    third commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,15 @@
 const links_array = Array.from(document.getElementsByClassName('menu__link'));
 
 links_array.forEach(element => {
-    element.onclick = () => {return false};
-    let el = element.closest('.menu__item');
-    Array.from(el.querySelector('menu').querySelectorAll('.menu__item')).forEach(element => {
-        element.classList.add('menu_active')
-    });
+    element.onclick = () => {
+        const el = element.closest('.menu__item');
+        if (el.querySelector('.menu') != null) { 
+            el.querySelector('.menu').classList.add('menu_active');
+        };
+        if (el.closest('.menu_sub') == null) {
+            return false;
+        };
+    };
 });
 
 
