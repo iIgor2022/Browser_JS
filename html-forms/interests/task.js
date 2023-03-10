2adff26c91788357207f8d1a65f6b12d898fafff
Igor Ovchinnikov <faceplaces@inbox.ru>
2023-03-10 15:43:34 +0300

    nineth commit

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,3 +1,21 @@
 const interest = document.querySelector('.interest');
+const checkboxes = Array.from(document.querySelectorAll('.interest__check'));
 
-console.log(interest.children.length)
+checkboxes.forEach(element => {
+    // f(element);
+    // console.log(element.checked);
+    element.addEventListener('change', function() {f(element)});
+});
+
+function f(el) {
+    const label = el.closest('label');
+    if (label.nextElementSibling) {
+        console.log(label.nextElementSibling.querySelectorAll('.interest__check'));
+        Array.from(label.nextElementSibling.querySelectorAll('.interest__check')).forEach(
+            element => {
+                element.checked = el.checked;
+                f(element);
+            }
+        );
+    }
+};
