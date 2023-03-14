f1f8e52dd96de15be22755dae77d55a7d5b35a42
Igor Ovchinnikov <faceplaces@inbox.ru>
2023-03-14 16:40:43 +0300

    eleventh commit

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,17 @@
+const href = Array.from(document.querySelectorAll('.has-tooltip'));
+
+href.forEach(element => {
+    element.onclick = function () {
+        if (document.querySelector('.tooltip_active')) {
+            document.querySelector('.tooltip_active').remove();
+        };
+        const div = document.createElement('div');
+        div.innerText = this.getAttribute('title');
+        div.classList.add('tooltip');
+        div.classList.add('tooltip_active');
+        div.style.left = `${Math.floor(this.getBoundingClientRect().left)}px`;
+        div.dataset.postion = "top";
+        this.appendChild(div);
+        return false;
+    }
+})
