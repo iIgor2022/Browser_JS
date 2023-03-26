99ce160c44c0999d77bf4761819dbba424417ca1
Овчинников Игорь <faceplaces@inbox.ru>
2023-03-26 13:40:42 +0300

    fourteenth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -9,14 +9,18 @@ document.querySelector('form').addEventListener('submit', function(event) {
     xhr.send(formData);
 });
 
+function showWelcome(user_id) {
+    document.querySelector('.signin').classList.remove('signin_active');
+    document.getElementById('user_id').innerText = user_id;
+    document.querySelector('.welcome').classList.add('welcome_active');
+};
+
 xhr.addEventListener('load', function() {
     if (this.status === 201) {
         const response = this.response;
         if (response['success']) {
             localStorage.setItem('auth', JSON.stringify({'user_id': response['user_id']}));
-            document.querySelector('.signin').classList.remove('signin_active');
-            document.getElementById('user_id').innerText = response['user_id'];
-            document.querySelector('.welcome').classList.add('welcome_active');
+            showWelcome(response['user_id']);
         } else {
             alert('Неверный логин/пароль!');
         };
@@ -34,3 +38,13 @@ document.getElementById('logout__btn').addEventListener('click', () => {
     document.querySelector('.signin').classList.add('signin_active');
     document.getElementById('user_id').innerText = '';
 });
+
+window.onload = () => {
+    if (localStorage.getItem('auth')) {
+        try {
+            showWelcome(JSON.parse(localStorage.getItem('auth'))['user_id']);
+        } catch (error) {
+            return false;    
+        };
+    }
+}
